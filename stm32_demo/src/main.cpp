d53e4a6db86d03658f692075b58f89401722a495
Marc Auger <marc@bubble.io>
2021-09-11 22:18:59 -0400

    flashing

--- stm32_demo/src/main.cpp
+++ stm32_demo/src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 
+#include "pins.h"
+
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(115200);
   Serial.flush();
@@ -8,5 +11,9 @@ void setup() {
 
 void loop() {
   Serial.println("it's working");
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 }
